8ac1e1f8656e3fc0e6f20ea333ed3eef3d39e763
xiaoguang <jiangroot@gmail.com>
2020-11-29 22:54:36 +0800

    cc

--- 1_TwoSum/s1.java
+++ 1_TwoSum/s1.java
@@ -2,4 +2,11 @@ class s1{
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length - 1; i++){
+            
+        }
+        return nums;
+    }
 }
