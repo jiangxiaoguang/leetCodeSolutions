e48fc3667d25c65e8f295cd2b6311cd399ec03f7
Shaoguang.Jiang <jiangroot@gmail.com>
2020-11-30 22:19:20 +0800

    2

--- 1_TwoSum/s1.java
+++ 1_TwoSum/s1.java
@@ -1,12 +1,21 @@
 class s1{
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] testa = new int[]{2, 7, 11, 15};
+        int[] ret = twoSum(testa, 26);
+        System.out.println(ret);
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
         for(int i = 0; i < nums.length - 1; i++){
-            
+           int toFind = target - nums[i];
+           for (int j = i + 1; j < nums.length; j++) {
+               if (nums[j] == toFind) {
+                   result[0] = i;
+                   result[1] = j;
+               }
+           }
         }
-        return nums;
+        return result;
     }
 }
