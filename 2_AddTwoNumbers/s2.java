155334ad17190b273a3d99e3728502845f50ac7a
Shaoguang.Jiang <jiangroot@gmail.com>
2020-11-30 22:26:56 +0800

    add

--- 2_AddTwoNumbers/s2.java
+++ 2_AddTwoNumbers/s2.java
@@ -8,9 +8,9 @@ public class s2 {
         ListNode cur = head;
         int carry = 0;
         do {
-            int sum = (l1 == null ? 0 : l1.val) + l2 == null ? 0 : l2.val;
-            carry = l1.val + l2.val > 10 ? 1 : 0;
-            cur.val = ret
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+            cur.val = (sum + carry) % 10;
+            carry = (sum + carry) > 10 ? 1 : 0;
             l1 = l1.next;
             l2 = l2.next;
         } while(l1 != null || l2 != null);
