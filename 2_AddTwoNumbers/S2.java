aed1b00cd98a9fc6dbb03e427511ccd2e4826839
Shaoguang.Jiang <jiangroot@gmail.com>
2021-01-05 22:54:44 +0800

    cc

--- 2_AddTwoNumbers/S2.java
+++ 2_AddTwoNumbers/S2.java
@@ -11,8 +11,9 @@ public class S2 {
             int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
             cur.val = sum % 10;
             carry = sum >= 10 ? 1 : 0;
-            l1 = l1.next;
-            l2 = l2.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            if (null == l1 && null == l2 && 0 == carry) break;
             ListNode next = new ListNode();
             cur.next = next;
             cur = next;
--- 3_LongestUnrepeateSubstring/S3.java
+++ 3_LongestUnrepeateSubstring/S3.java
@@ -1,12 +1,26 @@
+import java.util.HashSet;
+
 public class S3 {
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
 
     public int lengthOfLongestSubstring(String s) {
         int start = 0, end = 0;
+        HashSet<Character> cache = new HashSet<>();
         int max = 0;
-        
-        return 0;
+        while (end < s.length()) {
+            if (cache.contains(s.charAt(end))) {
+                cache.remove(s.charAt(start));
+                start++;
+                continue;
+            }
+            cache.add(s.charAt(end));
+            System.out.println(cache);
+            max = max > cache.size() ? max : cache.size();
+            end++;
+        }
+        return max;
     }
 }
